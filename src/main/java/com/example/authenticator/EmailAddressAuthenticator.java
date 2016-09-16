e245a51bef1184f4dc2fb7879d23ba38f244e308
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-09-16 11:57:25 +0100

    Simple Authenticator implementation that references the EmailAddressValidator and UserLocalService to validate user emails

--- src/main/java/com/example/authenticator/EmailAddressAuthenticator.java
+++ src/main/java/com/example/authenticator/EmailAddressAuthenticator.java
@@ -2,15 +2,17 @@ package com.example.authenticator;
 
 import java.util.Map;
 
+import com.example.authenticator.validator.EmailAddressValidator;
 import com.liferay.portal.kernel.security.auth.AuthException;
 import com.liferay.portal.kernel.security.auth.Authenticator;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	immediate = true,
-	property = {
-	},
+	property = {"key=auth.pipeline.post"},
 	service = Authenticator.class
 )
 public class EmailAddressAuthenticator implements Authenticator {
@@ -19,23 +21,43 @@ public class EmailAddressAuthenticator implements Authenticator {
 	public int authenticateByEmailAddress(long companyId, String emailAddress,
 			String password, Map<String, String[]> headerMap,
 			Map<String, String[]> parameterMap) throws AuthException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return validateDomain(emailAddress);
 	}
 
 	@Override
 	public int authenticateByScreenName(long companyId, String screenName,
 			String password, Map<String, String[]> headerMap,
 			Map<String, String[]> parameterMap) throws AuthException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String emailAddress = 
+			_userLocalService.fetchUserByScreenName(companyId, screenName).getEmailAddress();
+		
+		return validateDomain(emailAddress);
 	}
 
 	@Override
 	public int authenticateByUserId(long companyId, long userId,
 			String password, Map<String, String[]> headerMap,
 			Map<String, String[]> parameterMap) throws AuthException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String emailAddress = 
+			_userLocalService.fetchUserById(userId).getEmailAddress();
+		
+		return validateDomain(emailAddress);
 	}
+	
+	private int validateDomain(String emailAddress) throws AuthException {
+		
+		if (_emailValidator.isValidEmailAddress(emailAddress)) {		
+			return Authenticator.SUCCESS;
+		}
+		return Authenticator.FAILURE;
+	}
+	
+	@Reference
+	private volatile UserLocalService _userLocalService;
+	
+	@Reference  
+	private volatile EmailAddressValidator _emailValidator;	
 }
